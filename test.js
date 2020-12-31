ac0ad1c856f7095913e0c5fab21ebeac22ea751e
JoseAmarante12 <JoseAmarante12@users.noreply.github.com>
2020-12-30 22:21:25 -0500

    Addes files

---
+++ test.js
@@ -0,0 +1,43 @@
+function Clears()
+{
+    document.getElementById("input").value = '';
+}
+
+function Button(table,){
+
+}
+
+let x = 0;
+
+function Adds()
+{
+    let input = document.getElementById("input");
+
+       if(!input) 
+       {
+        alert("Must contain a value");
+       }
+       else 
+       {
+       let table = document.getElementById("table");
+       
+       let row = table.insertRow(-1);
+       let cell = row.insertCell(0);
+    
+       x++;
+       cell.innerHTML = "<div class='done' >" + x + ") " +input.value + " "+createButton()+"</div>";
+       }
+} 
+function createButton(){
+    let z = document.createElement("button");
+    return z.innerHTML ="<input type = 'image' src='trash.png' width='15'height='15' button onclick='completed()' id='done' ></button>";
+    
+}
+
+function completed(){
+
+    table = document.getElementById("table");
+    table.deleteRow(1);
+    x--;
+
+}
